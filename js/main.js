08119d7e39931ad30191f0de8a95fa4c57834e72
Oleg <107485701+sal1r@users.noreply.github.com>
2022-07-08 19:54:31 +0300

    Delete js directory

--- js/main.js
+++
@@ -1,4 +0,0 @@
-links = [["обо мне", "#about"], ["прайслист", "#price"], ["контакты", "#contact"]]
-links.forEach(el => {
-	document.getElementById("head_links").innerHTML += `<a class="head_link" href="${el[1]}">${el[0]}</a>`
-});

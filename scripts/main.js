80510e7a85eb75252dd4012915f3f17cf40e2f9d
Oleg <107485701+sal1r@users.noreply.github.com>
2022-07-11 11:43:11 +0300

    added adaptive welcome

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 links = [["обо мне", "#about"], ["прайслист", "#price"], ["контакты", "#contact"]]
 links.forEach(el => {
-	document.getElementById("head_links").innerHTML += `<a class="head_link" href="${el[1]}">${el[0]}</a>`
+	document.getElementById("head_links").innerHTML += `<a class="link" href="${el[1]}">${el[0]}</a>`
 });
 
 document.getElementById("alert_close").onclick = () => {
-	document.getElementById("alert").style.display = "none"
+	document.getElementById("alert").style.display = "none";
 }
